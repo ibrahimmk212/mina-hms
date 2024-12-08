f289580875e2fb87d79af45eebbbb6660f6ba348
Ibrahim <ibrahimmk212@gmail.com>
2024-12-08 08:01:41 +0100

    fix

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -19,7 +19,7 @@ class AdminDashboardController extends Controller
         $data['nurses_count'] = User::where('role', 'nurse')->count();
         $data['pharmacists_count'] = User::where('role', 'pharmacist')->count();
         $data['receptionists_count'] = User::where('role', 'receptionist')->count();
-        $data['lab_scientists_count'] = User::where('role', 'lab_technician')->count();
+        $data['lab_scientists_count'] = User::where('role', 'lab_scientist')->count();
         $data['sheduled_appointments_count'] = Appointments::where('doctor_id', Auth::user()->id)->count();
 
         return view('admin.dashboard', $data);
